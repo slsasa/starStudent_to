ae0e7c8e2366dc291305a777f4073034315f6517
APPLE <apple-ty@yellow.local>
2016-05-09 09:34:52 +0800

    个人中心新增页面

---
+++ www/js/stuPerson/my-dydamic/my-dynamic.js
@@ -0,0 +1,17 @@
+/**
+ * Created by apple-ty on 16-5-9.
+ */
+angular.module('starter')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('tabs.my-dynamic',{
+        url:'/my-dynamic',
+        views:{'stuPerson':{
+          templateUrl: 'templates/stuPerson/my-dynamic/my-dynamic.html',
+          controller: 'myDayamicCtrl'
+        }}
+      });
+  })
+  .controller('myDayamicCtrl',function($scope){
+
+  })
